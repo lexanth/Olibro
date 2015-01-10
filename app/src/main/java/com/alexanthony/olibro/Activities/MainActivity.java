15be83be5f599bf2d76a322b3105fbf0cd51e028
Alex Anthony <alex.anthony28991@gmail.com>
2015-01-10 20:24:27 +0000

    Revert "A few theme changes. Have broken the track list."
    This reverts commit 3fa1b7f4d357f38a165b19771469db7507d3dc9c.
    
    Conflicts:
    	app/src/main/res/layout/track_list.xml

--- app/src/main/java/com/alexanthony/olibro/Activities/MainActivity.java
+++ app/src/main/java/com/alexanthony/olibro/Activities/MainActivity.java
@@ -117,21 +117,16 @@ public class MainActivity extends BaseActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        if (super.onOptionsItemSelected(item)) return true;
-        else {
-            return false;
-        }
-    }
+		//menu item selected
+		switch (item.getItemId()) {
+		case R.id.action_end:
+			stopService(playIntent);
+			playerSrv=null;
+			System.exit(0);
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 	//method to retrieve track info from device
 	public void getTrackList(){
---
+++ app/src/main/java/com/alexanthony/olibro/ChapterDBHelper.java
@@ -0,0 +1,7 @@
+package com.alexanthony.olibro;
+
+/**
+ * Created by alex on 08/01/15.
+ */
+public class ChapterDBHelper {
+}
